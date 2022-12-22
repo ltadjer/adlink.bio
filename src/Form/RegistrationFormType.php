ab64733ff4295df1a442b138c25f9cd4ac82f3f6
laura-lpn <74555001+laura-lpn@users.noreply.github.com>
2022-12-22 11:55:53 +0100

    css3

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -19,23 +19,22 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('pseudo', TextType::class, ['attr' => ['placeholder' => 'Nom du site']])
-            ->add('slug', TextType::class, ['attr' => ['placeholder' => 'Slug']])
-            ->add('email', EmailType::class, ['attr' => ['placeholder' => 'Email']])
+            ->add('pseudo', TextType::class, ['attr' => ['placeholder' => 'Nom du site', 'class' => 'form-input-account']])
+            ->add('slug', TextType::class, ['attr' => ['placeholder' => 'Slug', 'class' => 'form-input-account']])
+            ->add('email', EmailType::class, ['attr' => ['placeholder' => 'Email', 'class' => 'form-input-account']])
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
                     new IsTrue([
                         'message' => 'Vous devez accepter les conditions d\'utilisation.',
                     ]),
-                ],
-                'label' => 'en cochant cette case j\'accepte les conditions d\'utilisation'
+                ]
             ])
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password', 'placeholder' => 'Mot de passe'],
+                'attr' => ['autocomplete' => 'new-password', 'placeholder' => 'Mot de passe', 'class' => 'form-input-account'],
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Entrer un mot de passe',
